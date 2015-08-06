7a4550076b986ca822d9da599a2e108abbc39b6d
Nathen Harvey <nharvey@chef.io>
2015-08-06 01:46:43 +0000

    Integration tests for apache

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 describe 'apache::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+  it 'serves up the home page' do
+    expect(command('curl http://localhost').exit_status).to eq 0
   end
+ 
+  it 'enables httpd to start on reboot' do
+    expect(service 'httpd').to be_enabled
+  end
+
+  it 'is listening on port 80' do
+    expect(port 80).to be_listening
+  end  
 end
